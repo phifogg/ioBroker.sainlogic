751aed4fdc3552c2cf0c85245462c60194fec571
Daniel Dräs <phifogg@users.noreply.github.com>
2020-04-25 15:46:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -66,18 +66,25 @@ class Sainlogic extends utils.Adapter {
                  }
                 });
 
+
+                webServer.on('error', this.server_error);
+
                 webServer.listen(this.config.port, this.config.bind);
             }
             catch (e) {
-                if (e.toString().includes('EACCES') && this.config.port <= 1024) {
-                    this.log.error(`node.js process has no rights to start server on the port ${port}.\n` +
-                        `Do you know that on linux you need special permissions for ports under 1024?\n` +
-                        `You can call in shell following scrip to allow it for node.js: "iobroker fix"`
-                    );
-                } else {
-                    this.log.error(`Cannot start server on ${this.config.bind || '0.0.0.0'}:${this.config.port}: ${e}`);
-                }
+                this.log.error('Something else went wrong on starting our Listener');
             }
+        }
+    }
+
+    server_error(e) {
+        if (e.toString().includes('EACCES') && this.config.port <= 1024) {
+            this.log.error(`node.js process has no rights to start server on the port ${port}.\n` +
+                `Do you know that on linux you need special permissions for ports under 1024?\n` +
+                `You can call in shell following scrip to allow it for node.js: "iobroker fix"`
+            );
+        } else {
+            this.log.error(`Cannot start server on ${this.config.bind || '0.0.0.0'}:${this.config.port}: ${e}`);
         }
     }
 
