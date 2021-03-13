c6636cb9ab656809817bba29a16ed9bb19a3fdfe
Daniel Dräs <phifogg@users.noreply.github.com>
2021-03-13 12:28:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ class Sainlogic extends utils.Adapter {
         });
         this.on('ready', this.onReady.bind(this));
         // this.on('objectChange', this.onObjectChange.bind(this));
-        this.on('stateChange', this.onStateChange.bind(this));
+        //this.on('stateChange', this.onStateChange.bind(this));
         // this.on('message', this.onMessage.bind(this));
         this.on('unload', this.onUnload.bind(this));
 
